f97e8dcac02256e8c1703ec8959239b6d8eda99b
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-07-27 22:48:01 +0900

    #6.0~6.1 절대경로/상대경로 & 조건문(ex: view, views)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,8 +12,8 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
+    views: 1,
+    id: 2,
   },
   {
     title: "Third Video",
@@ -21,14 +21,18 @@ let videos = [
     comments: 2,
     createdAt: "2 minutes ago",
     views: 59,
-    id: 1,
+    id: 3,
   },
 ];
 
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
