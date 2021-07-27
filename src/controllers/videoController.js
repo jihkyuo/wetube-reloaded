d39c3578382c292ee2c0cb08dd6f78677cff7e79
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-07-27 21:18:22 +0900

    Iteration & Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,35 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
-};
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+];
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
