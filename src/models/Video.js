913a2b9be78442f4633f1a7f1c021ce2029f2dda
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-08-08 00:06:20 +0900

    #6.23 upload 해쉬태그 미들웨어사용

--- src/models/Video.js
+++ src/models/Video.js
@@ -14,7 +14,7 @@ const videoSchema = new mongoose.Schema({
 videoSchema.pre("save", async function () {
   this.hashtags = this.hashtags[0]
     .split(",")
-    .map((word) => (word.startWith("#") ? word : `#${word}`));
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
