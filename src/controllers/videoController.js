66db0944a549b528c5ea3c55478b3bb39c938226
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-08-09 00:01:07 +0900

    # 6.26 sort / search
    mongoose에서 제공하는 sort기능
    model.find()에서 비디오를 찾고 정렬시킨다
    
    desc 내림차순
    asc 오름차순
    ---------------------------------
    req.query : input의 name으로 입력된 값을 연결하여, 검색한 값을 가져올 수 있다 (req.body를 쓰지 않는 이유는 query가 URL data이기 때문.)
    
    req.params : id를 가져온다
    req.body : post때, 입력된 값들을 가져온다

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -61,4 +61,12 @@ export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if (keyword) {
+    //search
+  }
+  return res.render("search", { pageTitle: "Search" });
 };
