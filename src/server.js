5bd38b7c3ca39a6c5227119348afad74e1a83328
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-08-11 23:19:23 +0900

    #7.10 response의 locals 접근
    pug 템플릿에서 어떻게 session 정보를 받을 수 있을까?
    => pug는 response의 local에 접근 가능하다(pug와 Express는 서로 locals를 공유하도록 설정 되어있다)
    
    pug에서 res.locals.sexy의 sexy만 변수로 주어도 locals에서 정보를 가져올 수 있다

--- src/server.js
+++ src/server.js
@@ -23,15 +23,11 @@ app.use(
 );
 
 app.use((req, res, next) => {
+  res.locals.sexy = "you";
   req.sessionStore.all((error, sessions) => {
     console.log(sessions);
     next();
   });
-});
-
-app.get("/add-one", (req, res, next) => {
-  req.session.potato += 1;
-  return res.send(`${req.session.id}\n${req.session.potato}`);
 });
 
 app.use("/", rootRouter);
