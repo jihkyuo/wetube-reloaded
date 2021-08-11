5a02bb0e15fd0d8e875cba1acd52f7e7f8828094
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-08-11 21:30:28 +0900

    #7.7 Session 설치
    1. npm 으로 express-session 설치
    2. server.js 에서 설치한 session import
    3. session 미들웨어 사용(에러 config 설정)
    4. 꼭 route 전에 코드 입력
    => 이 미들웨어가 로그인하지 않아도 사이트로 들어오는 모두를 기억하게 해준다
    => 들어오는 사람들한테 어떤 텍스트를 건냄
    = 백엔드의 미들웨어가 브라우저로 '쿠키'를 건낸다

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,6 +13,15 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
