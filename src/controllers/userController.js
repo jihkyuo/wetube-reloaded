e7e3fffe586209b8bcea9c4dac45be8f16e4079d
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-08-15 17:41:14 +0900

    #7.22 깃헙로그인 반복코드 정리

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -117,13 +117,9 @@ export const finishGithubLogin = async (req, res) => {
     if (!emailObj) {
       return res.redirect("/login");
     }
-    const existingUser = await User.findOne({ email: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect("/");
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
         name: userData.name ? userData.name : "Unknown",
         username: userData.login,
         email: emailObj.email,
@@ -131,10 +127,10 @@ export const finishGithubLogin = async (req, res) => {
         socialOnly: true,
         location: userData.location,
       });
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect("/");
     }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
