b6482301d9f738c4d6163d083ca5c83de4a1cbc9
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-08-16 22:42:39 +0900

    #8.5 비번 변경(로그아웃 시키기&세션업뎃)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -173,11 +173,12 @@ export const getChangePassword = (req, res) => {
 export const postChangePassword = async (req, res) => {
   const {
     session: {
-      user: { _id, password },
+      user: { _id },
     },
     body: { oldPassword, newPassword, newPasswordConfirmation },
   } = req;
-  const ok = await bcrypt.compare(oldPassword, password);
+  const user = await User.findById(_id);
+  const ok = await bcrypt.compare(oldPassword, user.password);
   if (!ok) {
     return res.status(400).render("users/change-password", {
       pageTitle: "Change Password",
@@ -190,10 +191,9 @@ export const postChangePassword = async (req, res) => {
       errorMessage: "The password does not match the confirmation",
     });
   }
-  const user = await User.findById(_id);
   user.password = newPassword;
   await user.save();
-  return res.redirect("/");
+  return res.redirect("/users/logout");
 };
 
 export const see = (req, res) => res.send("See User");
